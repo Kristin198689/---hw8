2e4d280f8b82cf7ea6fd6f675ed559156d7e6e61
Kristin198689 <163996836+Kristin198689@users.noreply.github.com>
2024-08-20 16:47:47 +0200

    NEW

--- Untitled-1.sql
+++ Untitled-1.sql
@@ -1,43 +1,52 @@
+-- Подключитесь к базе данных Students (которая находится на удаленном сервере).
+USE Students; 
+-- Найдите самого старшего студента
+SELECT first_name, age
+FROM Students
+ORDER BY age ASC
+LIMIT 1;
+
+-- Найдите самого старшего преподавателя
+
+SELECT name, age
+FROM Teachers
+ORDER BY age ASC
+LIMIT 1;
 
 
+-- Выведите список преподавателей с количеством компетенций у каждого
+select *
+from Teachers;
+select *
+from Teachers2Competencies;
 
---Найти самого старшего студента:
+SELECT 
+    t.name AS teacher_name, 
+    COUNT(tc.id) AS teacher_competencies_count,
+    GROUP_CONCAT(c.title ORDER BY c.title ASC SEPARATOR ', ') AS competencies_list
+FROM 
+    Teachers t
+LEFT JOIN 
+    Teachers2Competencies tc ON t.id = tc.teacher_id
+LEFT JOIN 
+    Competencies c ON tc.competencies_id = c.id
+GROUP BY 
+    t.name;
 
-
-USE students;
-USE Students;
-SELECT name, MAX(age) AS age
-FROM students;
-
-
---Найти самого старшего преподавателя:
-
-
-SELECT name, MAX(age) AS age
-FROM Teachers;
-
-
---Вывести список преподавателей с количеством компетенций у каждого:
-
-
-SELECT t.name, COUNT(c.skill_id) AS skill_count
+SELECT t.name, COUNT(tc.id) AS teacher_competncies
 FROM Teachers t
-LEFT JOIN competencies c ON t.id = c.teacher_id
-GROUP BY t.name;  
+LEFT JOIN Teachers2Competencies tc ON t.id = tc.teacher_id
+GROUP BY t.name;
 
+-- Выведите список курсов с количеством студентов в каждом
 
---Вывести список курсов с количеством студентов в каждом:
+SELECT c.title AS course_name, COUNT(sc.student_id) AS student_count
+FROM Courses c
+LEFT JOIN Students2Courses sc ON c.id = sc.course_id
+GROUP BY c.title;
 
-
-SELECT c.course_name, COUNT(sc.student_id) AS student_count
-FROM courses c
-LEFT JOIN student_courses sc ON c.id = sc.course_id
-GROUP BY c.course_name;
-
-
---Вывести список студентов с количеством курсов, посещаемых каждым студентом:
-
-SELECT s.name, COUNT(sc.course_id) AS course_count
-FROM students s
-LEFT JOIN student_courses sc ON s.id = sc.student_id
-GROUP BY s.name;subject; 
+-- Выведите список студентов, с количеством курсов, посещаемых каждым студентом.
+SELECT s.first_name , COUNT(sc.course_id) AS course_count
+FROM Students s
+LEFT JOIN Students2Courses sc ON s.id = sc.student_id
+GROUP BY s.first_name;
